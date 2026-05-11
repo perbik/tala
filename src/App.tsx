0a992dabd97fca1814520ca1bd1c2b936725615b
Fervicmar Lagman <fervicmardlagman@gmail.com>
2026-05-11 21:35:21 +0800

    fix: repair errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import type { Session } from '@supabase/supabase-js';
 import { LogOut, UserCircle } from 'lucide-react';
 import { supabase } from './lib/supabase';
